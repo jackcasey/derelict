0061d710eceb357207b27de51c14780ae9439687
Jack Casey <jack.casey@agworld.com.au>
2011-12-05 00:47:38 +0800

    Tweak return values of some methods. Added inspect to room for some more info while debugging.

--- lib/derelict/room.rb
+++ lib/derelict/room.rb
@@ -9,16 +9,22 @@ module Derelict
       @events = []
     end
 
-    def <<( actor )
+    def <<( *args )
+      add_actor( *args )
+    end
+
+    def add_actor( actor )
       @actors << actor
       actor.listeners << self # TODO careful of circular reference confusing GC
-      event Event.new( self, :enters, actor )
+      event( Event.new( self, :enters, actor ) )
+      actor
     end
 
     def event( e )
       raise ArgumentError.new("Argument must be an instance of Event") unless e.is_a? Event
       @events << e
       @actors.each {|x| x.hear( e.message )}
+      e
     end
 
     def event_messages
@@ -27,6 +33,10 @@ module Derelict
 
     def find_actor( name )
       @actors.select{|x| x.name == name}[0]
+    end
+
+    def inspect
+      "#{self}: #{@actors.count} actors."
     end
 
     def to_s
